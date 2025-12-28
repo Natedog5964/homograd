f941fecc3189fa90f70bd493c66c521a7d64b2d3
NateDog911 <Nathanpoop911@gmail.com>
2025-12-27 17:10:30 -0800

    Update init_sv.lua
    ROUND TIME TO 10 MINUTES

--- gamemodes/homigrad/gamemode/game/levels/ffa/init_sv.lua
+++ gamemodes/homigrad/gamemode/game/levels/ffa/init_sv.lua
@@ -3,7 +3,7 @@ function ffa.StartRoundSV()
     tdm.RemoveItems()
 
     roundTimeStart = CurTime()
-    roundTime = 300 + math.random(0, 300)
+    roundTime = 600 + math.random(0, 300)
 
     local players = PlayersInGame()
     for i, ply in ipairs(players) do
